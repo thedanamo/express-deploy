f9c8d683173f0c3c995f5a133ce9dd73fdfe87f6
Daniel <dan.hackl@gmail.com>
2019-05-03 17:15:05 -0400

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ let express = require("express");
 let app = express();
 app.get("/", (req, res) => {
   res.send(
-    "“I love scotch. Scotchy scotch scotch. Here it goes down, down into my belly. mm mm mm” — Ron Burgundy"
+    `<div>“I love scotch. Scotchy scotch scotch. Here it goes down, down into my belly. mm mm mm” — Ron Burgundy</div>
+    <div> another quote: </div>
+    <div>[talking to Baxter, his dog] Come again? You know I don't speak Spanish. In English, please. What? You pooped in the refrigerator? And you ate the whole... wheel of cheese? How'd you do that? I'm not even mad, that's amazing.  — also Ron Burgundy</div>`
   );
 });
 
